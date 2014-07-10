1fbe665156b96f70added507a7980d6710d68e5c
Joni Nousiainen <developer@joninousiainen.info>
2014-07-10 16:26:30 +0300

    Initial import.

---
+++ src/main/java/info/joninousiainen/examples/App.java
@@ -0,0 +1,18 @@
+package info.joninousiainen.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @EnableAutoConfiguration
+public class App {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
